cea5ac3d0cae0bfcf0bfa01889bca1c1c717793c
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-01-01 16:54:11 +0400

    added functionality to change background color in hex.html

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,5 +1,4 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];\
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+function randomHexCode() {
+  // get random number between from 0 to 3
+  const indexOne = Math.floor(Math.random() * hex.length);
+  const indexTwo = Math.floor(Math.random() * hex.length);
+  const indexThree = Math.floor(Math.random() * hex.length);
+  const indexFour = Math.floor(Math.random() * hex.length);
+  const indexFive = Math.floor(Math.random() * hex.length);
+  const indexSix = Math.floor(Math.random() * hex.length);
+
+  const hexCode = '#' + hex[indexOne] + hex[indexTwo] + hex[indexThree] + hex[indexFour] + hex[indexFive] + hex[indexSix];
+
+  return hexCode;
+}
+
+function changeColor() {
+  document.body.style.backgroundColor = randomHexCode();
+  color.textContent = randomHexCode();
+};
+
+btn.addEventListener('click', changeColor);
