f30139445bdf38f21a10bd054d74597fd7d281da
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-01-01 16:07:22 +0400

    added javascript code to change background color on clicking button

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+function changeColor() {
+  // console.log(document.body);
+  // get random number between from 0 to 3
+  const index = Math.floor(Math.random() * colors.length);
+  document.body.style.backgroundColor = colors[index];
+  color.textContent = colors[index];
+};
+
+btn.addEventListener('click', changeColor);
