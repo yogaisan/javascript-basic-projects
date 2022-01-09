e879fb95376672b2f980df9b3b1e34bdd3863260
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-01-09 15:16:42 +0400

    completed navbar

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,32 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const links = document.querySelector(".links");
+const navToggle = document.querySelector('.nav-toggle');
+
+function toggleLinks() {
+  if (links.classList.contains("show-links")) {
+    links.classList.remove('show-links');
+  } else {
+    links.classList.add('show-links');
+  }
+};
+
+navToggle.addEventListener('click', toggleLinks);
+
+
+// My alternative solution
+
+/*
+
+const links = document.querySelector(".links");
+const navToggle = document.querySelector('.nav-toggle');
+
+function toggleLinks() {
+  links.classList.toggle('show-links');
+};
+
+navToggle.addEventListener('click', toggleLinks);
+
+*/
