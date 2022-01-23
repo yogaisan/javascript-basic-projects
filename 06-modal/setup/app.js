66daa67e2490645eb1a20efb523cfd108cd15361
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-01-23 12:43:52 +0400

    completed modal

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,18 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+const closeBtn = document.querySelector('.close-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+function openModal() {
+  modalOverlay.classList.add('open-modal');
+};
+
+function closeModal() {
+  modalOverlay.classList.remove('open-modal');
+};
+
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal)
