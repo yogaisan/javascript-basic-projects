f429e4b004558712582d772ecf3afd9c4e203f5b
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-02-06 14:02:03 +0400

    completed questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,47 @@
-//using selectors inside the element
-// traversing the dom
+// ***using selectors inside the element***
+
+/*
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  const btn = question.querySelector('.question-btn');
+  // console.log(btn);
+  btn.addEventListener('click', () => {
+    questions.forEach((element => {
+      if (element !== question) {
+        element.classList.remove('show-text');
+      }
+      // console.log(element);
+      // console.log(question);
+    }));
+    question.classList.toggle('show-text');
+  });
+});
+*/
+
+// ***traversing the dom***
+
+const btns = document.querySelectorAll('.question-btn');
+const questions = document.querySelectorAll('.question');
+
+const toggleText = (event) => {
+  // console.log(event.currentTarget.parentElement.parentElement);
+  const question = event.currentTarget.parentElement.parentElement;
+
+  questions.forEach((element => {
+    if (element !== question) {
+      element.classList.remove('show-text');
+    }
+
+  }));
+
+  question.classList.toggle('show-text');
+
+  // if (question.classList.contains('show-text')) {
+  //   question.classList.remove('show-text');
+  // } else {
+  //   question.classList.add('show-text');
+  // }
+};
+
+btns.forEach(btn => btn.addEventListener('click', toggleText));
